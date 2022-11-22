dfdb96ca54ca2236d0cec5c910cddc9353897715
Ziyad <ziyad.awadh.alshamrani@gmail.com>
2022-11-22 19:46:45 +0300

    HHH

--- app.java
+++ app.java
@@ -1,6 +1,6 @@
 public class app{ 
 
     public static void main(String[] args) {
-        System.out.println("Hello this is new branch ");
+        System.out.println("Hello this is an update ");
     }
 }
